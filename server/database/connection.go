8c4ccd45847efbe3e9efdc83ed3706f99c78e7ef
unknown <rodrigue2014@yahoo.fr>
2022-07-11 09:18:44 -0700

    adding viper config management

--- server/database/connection.go
+++ server/database/connection.go
@@ -1,8 +1,6 @@
 package database
 
 import (
-	"fmt"
-
 	"github.com/pegdwende/VSM.git/env"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -14,14 +12,13 @@ var databaseConnectionEstablished = false
 
 func Connect() {
 
-	fmt.Printf("database %s", env.GetRequiredEnvVariable("VSM_DB_PASSWORD"))
 	dns := Dns{
-		username: env.GetRequiredEnvVariable("VSM_DB_USERNAME"),
-		password: env.GetRequiredEnvVariable("VSM_DB_PASSWORD"),
-		host:     env.GetRequiredEnvVariable("VSM_DB_HOST"),
-		port:     env.GetRequiredEnvVariable("VSM_DB_PORT"),
-		protocol: env.GetRequiredEnvVariable("VSM_DB_PROTOCOL"),
-		database: env.GetRequiredEnvVariable("VSM_DB_DATABASE"),
+		username: env.GetRequiredEnvVariable("MYSQL_USER"),
+		password: env.GetRequiredEnvVariable("MYSQL_PASSWORD"),
+		host:     env.GetRequiredEnvVariable("MYSQL_DB_HOST"),
+		port:     env.GetRequiredEnvVariable("MYSQL_DB_PORT"),
+		protocol: env.GetRequiredEnvVariable("MYSQL_DB_PROTOCOL"),
+		database: env.GetRequiredEnvVariable("MYSQL_DATABASE"),
 	}
 
 	connection, err := gorm.Open(mysql.Open(dns.getDnsString()), &gorm.Config{})
--- server/env/env.go
+++ server/env/env.go
@@ -2,20 +2,89 @@ package env
 
 import (
 	"fmt"
-	"os"
+	"log"
+
+	"github.com/spf13/viper"
 )
 
-func GetEnvVariable(name string, required bool) string {
-	return os.Getenv(name)
+var envLoaded = false
+
+func GetEnvVariable(name string) string {
+
+	loadEnv()
+
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type assertion, we know the underlying value is string
+	// if we type assert to other type it will throw an error
+	value, _ := viper.Get(name).(string)
+
+	return value
 }
 
 func GetRequiredEnvVariable(name string) string {
 
-	envValue := os.Getenv(name)
+	loadEnv()
 
-	if len(envValue) <= 0 {
+	// viper.Get() returns an empty interface{}
+	// to get the underlying type of the key,
+	// we have to do the type assertion, we know the underlying value is string
+	// if we type assert to other type it will throw an error
+	value, ok := viper.Get(name).(string)
+
+	if !ok {
 		panic(fmt.Sprintf("Could not find environment variable value for %s environment variable", name))
 	}
 
-	return envValue
+	return value
 }
+
+func loadEnv() {
+
+	if envLoaded == false {
+		// SetConfigFile explicitly defines the path, name and extension of the config file.
+		// Viper will use this and not check any of the config paths.
+		// .env - It will search for the .env file in the current directory
+		viper.SetConfigFile("../.env")
+
+		// Find and read the config file
+		err := viper.ReadInConfig()
+
+		if err != nil {
+			log.Fatalf("Error while reading config file %s", err)
+		}
+
+		envLoaded = true
+	}
+}
+
+// use viper package to read .env file
+// return the value of the key
+// func viperEnvVariable(key string) string {
+
+// 	// SetConfigFile explicitly defines the path, name and extension of the config file.
+// 	// Viper will use this and not check any of the config paths.
+// 	// .env - It will search for the .env file in the current directory
+// 	viper.SetConfigFile(".env")
+
+// 	// Find and read the config file
+// 	err := viper.ReadInConfig()
+
+// 	if err != nil {
+// 		log.Fatalf("Error while reading config file %s", err)
+// 	}
+
+// 	// viper.Get() returns an empty interface{}
+// 	// to get the underlying type of the key,
+// 	// we have to do the type assertion, we know the underlying value is string
+// 	// if we type assert to other type it will throw an error
+// 	value, ok := viper.Get(key).(string)
+
+// 	// If the type is a string then ok will be true
+// 	// ok will make sure the program not break
+// 	if !ok {
+// 		log.Fatalf("Invalid type assertion")
+// 	}
+
+// 	return value
+// }
